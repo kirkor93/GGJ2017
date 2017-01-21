2d89b8033c3a38817e8183545d2d1b0b86cfba84
Marcek <wtmarcek@gmail.com>
2017-01-21 11:58:22 +0100

    echolocation script

---
+++ Assets/Scripts/Bat/Echolocation.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Echolocation : MonoBehaviour {
+
+	public Light echoLightPrefab;
+	private GameObject plain;
+
+	public float echoBar = 100.0f;
+	private float resetEchoBar;
+
+	private bool canSuper;
+
+	// Use this for initialization
+	void Start () {
+		resetEchoBar = echoBar;
+		plain = GameObject.FindGameObjectWithTag("Plain");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		EchoLoads ();
+	}
+
+	void EchoLoads(){
+		echoBar += Time.deltaTime;
+		if (echoBar == resetEchoBar) {
+			canSuper = true;
+		} else {
+			canSuper = false;
+		}
+
+		//Miejsce na muchy
+	}
+
+	void SuperEcho(){
+		if (canSuper) {
+			//GameObject echoLightClone = Instantiate(echoLightPrefab, new Vector3 (0,plain.gameObject.transform.position.z,0 + 0.1f,0), Quaternion.
+		}
+	}
+}
